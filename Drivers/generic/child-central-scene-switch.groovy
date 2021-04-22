8eab5bb50d091c95d91daaee4300929c68510227
Jeff Page <jtp10181@gmail.com>
2021-04-21 19:29:48 -0500

    Update child-central-scene-switch.groovy
    Adding functions to handle commands added in hub firmware 2.2.6

--- Drivers/generic/child-central-scene-switch.groovy
+++ Drivers/generic/child-central-scene-switch.groovy
@@ -7,12 +7,12 @@
     NOTICE: This file has been modified by *Jeff Page*
         from the original work of *Mike Maxwell (Hubitat)*.
 
-    Copyright 2020 Jeff Page
+    Copyright 2021 Jeff Page
     Copyright 2016 -> 2020 Hubitat Inc. All Rights Reserved
 
     Changelog:
 
-## [1.0.0] - 2021-03-12 (@jtp10181)
+## [1.0.0] - 2021-04-21 (@jtp10181)
   - Initial release
 
 */
@@ -21,6 +21,7 @@ import groovy.transform.Field
 
 metadata {
     definition(
+        //If name is changed must also be changed in parent driver addChildDevice
         name: "Child Central Scene Switch", 
         namespace: "jtp10181", 
         author: "Jeff Page", 
@@ -32,12 +33,24 @@ metadata {
         capability "PushableButton"
         capability "HoldableButton"
         capability "ReleasableButton"
-        capability "DoubleTapableButton"
+        //capability "DoubleTapableButton"
     }
     preferences {
         //Logging options similar to other Hubitat drivers
         input name: "txtEnable", type: "bool", title: "Enable Description Text Logging?", defaultValue: false
     }
+}
+
+void push(buttonId) { sendBasicButtonEvent(buttonId, "pushed") }
+void hold(buttonId) { sendBasicButtonEvent(buttonId, "held") }
+void release(buttonId) { sendBasicButtonEvent(buttonId, "released") }
+void doubleTap(buttonId) { sendBasicButtonEvent(buttonId, "doubleTapped") }
+
+void sendBasicButtonEvent(BigDecimal buttonId, String name) {
+    Map event = [name: name, value: buttonId, type:"digital", isStateChange:true]
+    event.descriptionText="button ${buttonId} ${name}"
+    logTxt "${event.descriptionText} (${event.type})"
+    sendEvent(event)
 }
 
 void updated() {
